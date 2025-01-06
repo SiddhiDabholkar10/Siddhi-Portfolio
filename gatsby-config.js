5c73246e5794f3d1da0bb6ea85615f4acc8dc113
Siddhi Dabholkar <dabholkarsiddhi10@gmail.com>
2025-01-05 18:13:51 -0600

    Logo Name Updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/SiddhiLogo.png',
+        icon: 'src/images/siddhilogo.png',
       },
     },
     `gatsby-plugin-offline`,
